f7dbab7b23bb608fa01c117ebd167bf6d5df5497
FacuAce <acevedofacundo410@gmail.com>
2022-11-22 17:07:14 -0300

    Creacion de modulo de checkout para los elmentos agregados al carrito, agregado de tabla de detalla de elementos, Boton para el onSubmit con validacion de campos, Servicios y seleccion de stores en caso de no delivery, Creacion de los servicios y interfaces para las orders con sus detalles, logica al onSubmit para obtener los datos del formulario, thank you page luego de realizar la compra y control de stock

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,20 @@
+
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  template: `
+  <mat-toolbar class="main-header" color="primary">
+    <a [routerLink]="['/']">My Store</a>
+    <app-cart class="shopping-cart" (click)="goToCheckout()"></app-cart>
+  </mat-toolbar>`,
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private router: Router) { }
+
+  goToCheckout(): void {
+    this.router.navigate(['/checkout']);
+  }
+}
